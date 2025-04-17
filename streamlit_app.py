5c8b014e93569b817f5ffa5f592a8c1ad3d82cda
Nandhik-kk <jedaiyo23@gmail.com>
2025-04-17 10:57:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+# 💖 LoveLines: Code With Heart 💻🩷
+
+> 💡 Made with passion, powered by coffee ☕ and love 💘
+
+## ✨ Features that’ll make your heart flutter:
+- 💕 Smooth UI
+- 💌 Lovingly written code
+- 🧠 Smart & Sexy logic
+- 🐛 Zero heartbreak from bugs
 
 st.title("Kalkulator cinta :heart:")
 st.title("_azlina_ sayang :red[danapati] :heart::")

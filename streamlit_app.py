22aa879bd2772b8b48b0ec3ce3f4d6d4b8ac48ca
Nandhik-kk <jedaiyo23@gmail.com>
2025-04-17 10:53:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("Kalkulator Z")
-st.title("_azlina_ sayang :red[arya] :heart::")
+st.title("Kalkulator cinta :heart::")
+st.title("_azlina_ sayang :red[danapati] :heart::")
 st.write(
     "Ayo berhitung, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
